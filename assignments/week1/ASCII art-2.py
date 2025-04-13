1c3cd856b8dde893907999da70d714e338d9ddb4
lennardkoepke <lennard.koepke@stud.leuphana.de>
2025-04-13 19:53:52 +0200

    Add files via upload

---
+++ assignments/week1/ASCII art-2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("          ______")
+print("         /|_||_\`.__")
+print("        (   _    _ _\\")
+print("        =`-(_)--(_)-'")
+print("           VROOM!")
+
